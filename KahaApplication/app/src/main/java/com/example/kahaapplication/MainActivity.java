6d672fced4beb91e56f81bed4428e1325625b97f
Jacob Darvin <jacob_darvin@dlsu.edu.ph>
2021-08-20 11:38:51 +0800

    added registration

--- KahaApplication/app/src/main/java/com/example/kahaapplication/MainActivity.java
+++ KahaApplication/app/src/main/java/com/example/kahaapplication/MainActivity.java
@@ -3,16 +3,31 @@ package com.example.kahaapplication;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView registerText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+        loginButton = findViewById(R.id.btn_login);
+        registerText = findViewById(R.id.tv_register_link);
+
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
