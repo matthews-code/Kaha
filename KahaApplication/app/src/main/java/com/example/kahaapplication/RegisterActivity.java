825316c549a7f62c4996315532ad034bf1e49824
Darvvvin <jacob_darvin@dlsu.edu.ph>
2021-08-21 13:42:23 +0800

    xml edits

--- KahaApplication/app/src/main/java/com/example/kahaapplication/RegisterActivity.java
+++ KahaApplication/app/src/main/java/com/example/kahaapplication/RegisterActivity.java
@@ -3,12 +3,25 @@ package com.example.kahaapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ImageButton ibBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.ibBack = findViewById(R.id.ib_back);
+
+        this.ibBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
