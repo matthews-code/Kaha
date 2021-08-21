507ed85546619e8287b4ae75272125352f62d9ef
Darvvvin <jacob_darvin@dlsu.edu.ph>
2021-08-21 14:31:34 +0800

    added space widget

---
+++ KahaApplication/app/src/main/java/com/example/kahaapplication/FinderHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.kahaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinderHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finder_home);
+    }
+}
