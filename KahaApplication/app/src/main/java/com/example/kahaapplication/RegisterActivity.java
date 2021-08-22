dddaeff24ce3557d5af2bfdda6cd1a83b7579e0d
Darvvvin <jacob_darvin@dlsu.edu.ph>
2021-08-22 12:46:41 +0800

    added bday picker

--- KahaApplication/app/src/main/java/com/example/kahaapplication/RegisterActivity.java
+++ KahaApplication/app/src/main/java/com/example/kahaapplication/RegisterActivity.java
@@ -2,26 +2,62 @@ package com.example.kahaapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class RegisterActivity extends AppCompatActivity {
+    private TextView tvDate;
+    private DatePickerDialog.OnDateSetListener setListener;
 
     private ImageButton ibBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.register_activity);
 
         this.ibBack = findViewById(R.id.ib_back);
-
         this.ibBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+
+        this.tvDate = findViewById(R.id.tv_reg_date);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        RegisterActivity.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month+1;
+                String date = day+"/"+month+"/"+year;
+                tvDate.setText(date);
+            }
+        };
+
     }
 }
