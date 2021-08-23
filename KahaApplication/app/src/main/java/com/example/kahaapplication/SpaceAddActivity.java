2227388568b6c3a04bf9630bba59dd53f960ffa4
Darvvvin <jacob_darvin@dlsu.edu.ph>
2021-08-23 22:54:21 +0800

    image upload

--- KahaApplication/app/src/main/java/com/example/kahaapplication/SpaceAddActivity.java
+++ KahaApplication/app/src/main/java/com/example/kahaapplication/SpaceAddActivity.java
@@ -1,14 +1,81 @@
 package com.example.kahaapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+import com.squareup.picasso.Picasso;
 
 public class SpaceAddActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button btnChooseImage;
+    private Button btnCreateSpace;
+    private ProgressBar pbUploadStatus;
+    private ImageView ivThumb;
+
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_space_add);
+
+        Spinner spinner = (Spinner) findViewById(R.id.space_type_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spaces_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        this.btnChooseImage = findViewById(R.id.btn_upload);
+        this.btnCreateSpace = findViewById(R.id.btn_create);
+        this.pbUploadStatus = findViewById(R.id.pb_upload_status);
+        this.ivThumb = findViewById(R.id.iv_thumb_create);
+
+        btnChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
+
+        btnCreateSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+        && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Picasso.get().load(mImageUri).into(ivThumb);
+        }
+    }
+
 }
