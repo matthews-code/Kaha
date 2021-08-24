55ebad0fb721048359be0c099c39f7b7542c3cd2
Matthew Buensalida <matthew_buensalida@dlsu.edu.ph>
2021-08-24 19:31:34 +0800

    Map view

--- KahaApplication/app/src/main/java/com/example/kahaapplication/SpaceViewActivity.java
+++ KahaApplication/app/src/main/java/com/example/kahaapplication/SpaceViewActivity.java
@@ -16,7 +16,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 
 import java.security.Key;
 
-public class SpaceViewActivity extends AppCompatActivity{
+public class SpaceViewActivity extends AppCompatActivity implements OnMapReadyCallback{
     private ImageView ivThumbnail;
 
     private TextView tvSize;
@@ -27,6 +27,7 @@ public class SpaceViewActivity extends AppCompatActivity{
     private MapView mapView;
 
     private AppCompatButton btnContact;
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +45,21 @@ public class SpaceViewActivity extends AppCompatActivity{
 
         this.btnContact = findViewById(R.id.btn_contact);
 
+        initMap(savedInstanceState);
         retrieveData();
 
+    }
+
+    private void initMap(Bundle savedInstanceState) {
+        Bundle mapViewBundle = null;
+        if(savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        this.mapView = findViewById(R.id.mv_show_location);
+        mapView.onCreate(mapViewBundle);
+
+        mapView.getMapAsync(this);
     }
 
     private void retrieveData() {
@@ -68,4 +82,59 @@ public class SpaceViewActivity extends AppCompatActivity{
         String sLocation = i.getStringExtra(Keys.KEY_SPACE_LOCATION.name());
         this.tvTitle.setText(sType + " in " + sLocation);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
 }
