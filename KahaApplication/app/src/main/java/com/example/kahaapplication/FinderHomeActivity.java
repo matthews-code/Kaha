fa167f4d233783cd2363f09384f0a6c72b6a275e
Matthew Buensalida <matthew_buensalida@dlsu.edu.ph>
2021-08-25 02:50:51 +0800

    Login and changed views depending on type of user

--- KahaApplication/app/src/main/java/com/example/kahaapplication/FinderHomeActivity.java
+++ KahaApplication/app/src/main/java/com/example/kahaapplication/FinderHomeActivity.java
@@ -2,6 +2,7 @@ package com.example.kahaapplication;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.widget.NestedScrollView;
@@ -16,13 +17,23 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class FinderHomeActivity extends AppCompatActivity implements FinderHomeAdapter.OnSpaceListener{
     private ArrayList<SpaceModel> data;
+
+    private TextView tvHeader;
 
     private ImageButton ibBack;
     private NestedScrollView nsvFinderHome;
@@ -32,12 +43,23 @@ public class FinderHomeActivity extends AppCompatActivity implements FinderHomeA
 
     private FloatingActionButton fabAddSpace;
 
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
+    private String userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finder_home);
 
+        this.initComponents();
+        this.initFirebase();
+    }
+
+    private void initComponents () {
         this.data = new DataHelper().initData();
+
+        this.tvHeader = findViewById(R.id.tv_listing_header);
 
         this.recyclerView = findViewById(R.id.rv_listings);
         this.nsvFinderHome = findViewById(R.id.nsv_finder_home);
@@ -63,6 +85,41 @@ public class FinderHomeActivity extends AppCompatActivity implements FinderHomeA
                 startActivity(intent);
             }
         });
+    }
+
+    private void initFirebase() {
+        this.mAuth = FirebaseAuth.getInstance();
+        this.user = FirebaseAuth.getInstance().getCurrentUser();
+        this.userId = this.user.getUid();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference(Keys.COLLECTIONS_USERS.name());
+
+        //this.pbProfile.setVisibility(View.VISIBLE);
+        reference.child(this.userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                setViews(snapshot.child("userIsFinder").getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //pbProfile.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void setViews(String isFinder) {
+
+        if(isFinder.equalsIgnoreCase("true")) {
+            this.tvHeader.setText("Search for");
+            this.fabAddSpace.setVisibility(View.GONE);
+        } else {
+            this.tvHeader.setText("Your");
+            this.fabAddSpace.setVisibility(View.VISIBLE);
+        }
+
     }
 
     @Override
--- KahaApplication/app/src/main/java/com/example/kahaapplication/LoginActivity.java
+++ KahaApplication/app/src/main/java/com/example/kahaapplication/LoginActivity.java
@@ -1,25 +1,56 @@
 package com.example.kahaapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText etEmail;
+    private EditText etPassword;
+
     private TextView registerText;
     private Button loginButton;
+
+    private ProgressBar pbLogin;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        loginButton = findViewById(R.id.btn_register);
-        registerText = findViewById(R.id.tv_register_link);
+        this.initFirebase();
+        this.initComponents();
+
+    }
+
+    private void initFirebase(){
+        this.mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void initComponents() {
+
+        this.etEmail = findViewById(R.id.et_login_email);
+        this.etPassword = findViewById(R.id.et_login_password);
+        this.loginButton = findViewById(R.id.btn_register);
+        this.registerText = findViewById(R.id.tv_register_link);
+
+        this.pbLogin = findViewById(R.id.pb_login);
 
         registerText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,9 +63,49 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, FinderHomeActivity.class);
-                startActivity(intent);
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if(!checkEmpty(email, password)) {
+                    signIn(email, password);
+                }
             }
         });
+    }
+
+    public void signIn(String email, String password) {
+        this.pbLogin.setVisibility(View.VISIBLE);
+
+        this.mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        pbLogin.setVisibility(View.GONE);
+                        if(task.isSuccessful()) {
+                            pbLogin.setVisibility(View.GONE);
+                            Intent i = new Intent(LoginActivity.this, FinderHomeActivity.class);
+                            startActivity(i);
+                            finish();
+                        } else {
+                            failedRegistration();
+                        }
+                    }
+                });
+    }
+
+    private void failedRegistration() {
+        this.pbLogin.setVisibility(View.GONE);
+        Toast.makeText(this, "User login failed.", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean checkEmpty(String email, String password) {
+        boolean hasEmpty = false;
+
+        if(email.isEmpty() || password.isEmpty()) {
+            hasEmpty = true;
+            Toast.makeText(this, "Accomplish all fields", Toast.LENGTH_SHORT).show();
+        }
+
+        return hasEmpty;
     }
 }
