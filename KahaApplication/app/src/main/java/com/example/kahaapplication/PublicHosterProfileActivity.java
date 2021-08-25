6910f0722446a1323ef0ab8bd7846dc192bc3654
Matthew Buensalida <matthew_buensalida@dlsu.edu.ph>
2021-08-25 17:47:14 +0800

    Dynamic public hoster profile

--- KahaApplication/app/src/main/java/com/example/kahaapplication/PublicHosterProfileActivity.java
+++ KahaApplication/app/src/main/java/com/example/kahaapplication/PublicHosterProfileActivity.java
@@ -1,13 +1,27 @@
 package com.example.kahaapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageTask;
 
 import java.util.ArrayList;
+
+import okhttp3.internal.cache.DiskLruCache;
 
 public class PublicHosterProfileActivity extends ToolBarActivity implements FinderHomeAdapter.OnSpaceListener{
 
@@ -15,14 +29,73 @@ public class PublicHosterProfileActivity extends ToolBarActivity implements Find
     private RecyclerView recyclerView;
     private FinderHomeAdapter adapter;
 
+    private TextView fullName;
+    private TextView contactNumber;
+    private TextView emailAddress;
+    private TextView spacesHeader;
+
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
+    private String userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_hoster_profile);
 
         initToolbar();
+        this.initComponents();
+        this.initFirebase();
+    }
+
+    private void initFirebase() {
+        this.mAuth = FirebaseAuth.getInstance();
+        this.user = FirebaseAuth.getInstance().getCurrentUser();
+        this.userId = this.user.getUid();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference(Keys.COLLECTIONS_USERS.name());
+
+        //this.pbProfile.setVisibility(View.VISIBLE);
+        reference.child(this.userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                setViews(snapshot);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //pbProfile.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void setViews(DataSnapshot snapshot) {
+        String fullName = snapshot.child("userFirstName").getValue().toString() + " " + snapshot.child("userLastName").getValue().toString();
+        String firstName = snapshot.child("userFirstName").getValue().toString();
+        String spaceHeader;
+        if(Character.toLowerCase(firstName.charAt(firstName.length() - 1)) == 's') {
+            spaceHeader = firstName + "' spaces";
+        } else {
+            spaceHeader = firstName + "'s spaces";
+        }
+
+        this.fullName.setText(fullName);
+        this.contactNumber.setText(snapshot.child("userPhone").getValue().toString());
+        this.emailAddress.setText(snapshot.child("userEmail").getValue().toString());
+        this.spacesHeader.setText(spaceHeader);
+
+    }
+
+    private void initComponents () {
         this.data = new DataHelper().initData();
         this.recyclerView = findViewById(R.id.rv_user_spaces);
+
+        this.fullName = findViewById(R.id.tv_show_hoster_name);
+        this.contactNumber = findViewById(R.id.tv_show_hoster_contact);
+        this.emailAddress = findViewById(R.id.tv_show_hoster_email);
+        this.spacesHeader = findViewById(R.id.tv_show_hoster_spaces);
 
         this.adapter = new FinderHomeAdapter(data,  this);
         this.recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false));
